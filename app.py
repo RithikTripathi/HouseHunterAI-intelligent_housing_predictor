fe544dc0d85baa3875b113557885dffe76c58ee4
RithikTripathi <rkt7k2@gmail.com>
2022-08-20 22:27:35 +0530

    Exception Configured

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import Housing_Exception
+import sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    # to check exception is working properly, raising a exception
+    try:
+        raise Exception("False Exception : Testing Custom Exception...")
+    except Exception as e:
+        housing = Housing_Exception(e,sys)
+        logging.info("False Exception : Testing Custom Exception...")
+
     logging.info("Testing Logging Module...")
     return 'CI/CD Pipeline has been Estalished!!!'
 
