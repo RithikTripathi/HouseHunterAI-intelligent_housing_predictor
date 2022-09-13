83b309cab12607999454a3b7b9066072ebb3e199
RithikTripathi <rkt7k2@gmail.com>
2022-09-14 00:08:07 +0530

    Updated Configuration : get_data_ingestion_config() done

--- setup.py
+++ setup.py
@@ -17,8 +17,15 @@ def get_reuqirements_list()->List[str]:
     This function is responsible to return list of requirements as strings mentioned in requirements.txt
     Return : list containing name of libraries mentioned in requirements.txt
     """
-    with open(REQUIREMENTS_FILE_NAME, "r", encoding="utf-8") as requirement_file:
-        return requirement_file.readlines()
+    # with open(REQUIREMENTS_FILE_NAME, "r", encoding="utf-8") as requirement_file:
+    #     return requirement_file.readlines()
+
+    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 
 # Setting up setup()
